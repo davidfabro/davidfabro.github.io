ca29336e48c69f5dd8ebc00f118ff41cf1aee6c0
davidfabro <75782602+davidfabro@users.noreply.github.com>
2021-05-17 09:22:17 +0200

    Update leerFicheroXML.js

--- leerFicheroXML.js
+++ leerFicheroXML.js
@@ -2,10 +2,10 @@ function gestionarFicheroXML(xmlDoc){
 	let capaVacia = document.querySelector("#ficheroXML")
 	let libros = xmlDoc.querySelectorAll("libro")
 	for(let i=0; i<libros.length; i++)
-		if (i == 2)
-		capaVacia.innerHTML = capaVacia.innerHTML + "<p class='seleccionado'>" + libros[i].textContent + "</p>"
+		if (i%2 = 0)
+		capaVacia.innerHTML = capaVacia.innerHTML + "<p class='par'>" + libros[i].textContent + "</p>"
 	else
-		capaVacia.innerHTML = capaVacia.innerHTML + "<p class='borde'>" + libros[i].textContent + "</p>"
+		capaVacia.innerHTML = capaVacia.innerHTML + "<p class='impar'>" + libros[i].textContent + "</p>"
 	
 }
 

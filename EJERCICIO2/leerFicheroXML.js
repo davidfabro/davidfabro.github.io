d453b536977c570c9dd81caf5d253fc5fd06403e
davidfabro <75782602+davidfabro@users.noreply.github.com>
2021-05-19 16:46:16 +0200

    Add files via upload

---
+++ EJERCICIO2/leerFicheroXML.js
@@ -0,0 +1,13 @@
+function gestionarFicheroXML(xmlDoc){
+	let capaVacia = document.querySelector("#ficheroXML")
+	let libros = xmlDoc.querySelectorAll("libro")
+	for(let i=0; i<libros.length; i++)
+		if (i%2 = 0)
+		capaVacia.innerHTML = capaVacia.innerHTML + "<p class='par'>" + libros[i].textContent + "</p>"
+	else
+		capaVacia.innerHTML = capaVacia.innerHTML + "<p class='impar'>" + libros[i].textContent + "</p>"
+	
+}
+
+
+	loadDocA("libros.xml","xml");

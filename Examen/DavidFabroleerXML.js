25c45be36db134480823bf9217b839c349eff81c
davidfabro <75782602+davidfabro@users.noreply.github.com>
2021-05-21 09:46:02 +0200

    Update DavidFabroleerXML.js

--- Examen/DavidFabroleerXML.js
+++ Examen/DavidFabroleerXML.js
@@ -3,7 +3,7 @@ function gestionarFicheroXML(xmlDoc){
 	let cine = xmlDoc.querySelectorAll("pelicula")
 	for(let i=0; i<cine.length; i++)
 	
-		capaVacia.innerHTML = capaVacia.innerHTML + "<p class='par'>" + cine[i].textContent + "</p>
+		capaVacia.innerHTML = capaVacia.innerHTML + cine[i].textContent 
 	
 }
 function CargarFichero()

fbdfad6c3edf5193e774db97145d9f873f693947
davidfabro <75782602+davidfabro@users.noreply.github.com>
2021-05-21 09:33:45 +0200

    Update davidfabro leerXML.js

--- Examen/davidfabro leerXML.js
+++ Examen/davidfabro leerXML.js
@@ -1,16 +1,14 @@
 function gestionarFicheroXML(xmlDoc){
 	let capaVacia = document.querySelector("#ficheroXML")
-	let cine = xmlDoc.querySelectorAll("mensaje")
+	let cine = xmlDoc.querySelectorAll("pelicula")
 	for(let i=0; i<cine.length; i++)
-		if (i % 2 == 0)
-		capaVacia.innerHTML = capaVacia.innerHTML + "<p class='par'>" + cine[i].textContent + "</p>"
-	else
-		capaVacia.innerHTML = capaVacia.innerHTML + "<p class='impar'>" + cine[i].textContent + "</p>"
+	
+		capaVacia.innerHTML = capaVacia.innerHTML + "<p class='par'>" + cine[i].textContent + "</p>
 	
 }
 function CargarFichero()
 {
-	loadDocA("mensajes.xml","xml")
+	loadDocA("peliculas.xml","xml")
 	
 }
 	setInterval(CargarFichero, 5000);
